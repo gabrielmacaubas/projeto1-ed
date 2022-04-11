27ed4c590491855df05db2146d54e36255c1defa
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-04-11 20:14:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -35,33 +35,39 @@ while True:
         rodadas += 1
         print(f"\n{rodadas}º rodada ->")
         
-        ganhador = str()
-        carta1_valor = j1.topo().valor
-        carta2_valor = j2.topo().valor
+        while True:
+            ganhador = str()
+            carta1_valor = j1.topo().valor
+            carta2_valor = j2.topo().valor
 
-        if carta1_valor >= carta2_valor:
-            ganhador = j1.getNome()
-        else:
-            ganhador = j2.getNome()
+            cp.empilha(j1.jogarCarta())
+            print(f"\nO jogador {j1.getNome()} jogou a carta {cp.topo()}")
+            cp.empilha(j2.jogarCarta())
+            print(f"O jogador {j2.getNome()} jogou a carta {cp.topo()}")
 
-        cp.empilha(j1.jogarCarta())
-        print(f"\nO jogador {j1.getNome()} jogou a carta {cp.topo()}")
-        cp.empilha(j2.jogarCarta())
-        print(f"O jogador {j2.getNome()} jogou a carta {cp.topo()}")
+            if carta1_valor > carta2_valor:
+                ganhador = j1.getNome()
+                print(f"O jogador {ganhador} venceu e recebeu as cartas:", end=" ")
+                cp.imprime()
 
-        if ganhador == j1.getNome():
-            print(f"O jogador {ganhador} venceu e recebeu as cartas:", end=" ")
-            cp.imprime()
-            for i in range(cp.tamanho()):
-                j1.colocaBase(cp.desempilha())
+                for i in range(cp.tamanho()):
+                    j1.colocaBase(cp.desempilha())
+                
+                break
+
+            elif carta2_valor > carta1_valor:
+                ganhador = j2.getNome()
+                print(f"O jogador {ganhador} venceu e recebeu as cartas:", end=" ")
+                cp.imprime()    
+
+                for i in range(cp.tamanho()):
+                    j2.colocaBase(cp.desempilha()) 
+
+                break
             
-        else:
-            print(f"O jogador {ganhador} venceu e recebeu as cartas:", end=" ")
-            cp.imprime()    
-            for i in range(cp.tamanho()):
-                j2.colocaBase(cp.desempilha()) 
-
-
+            print("Ops! Deu empate. Mais duas cartas na mesa!")
+            continuar = input("Aperte [ENTER] para continuar")
+                
         print(f"O jogador {j1.getNome()} agora tem {j1.quantidadeCartas()} cartas")
         print(f"O jogador {j2.getNome()} agora tem {j2.quantidadeCartas()} cartas")
         continuar = input("Aperte [ENTER] para continuar")
